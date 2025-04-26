969a3876cf34974cd8a120d4714c713c948ddca4
saurabhje <work.saurabh@outlook.com>
2025-04-27 03:45:22 +0530

    Everything Rosey

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<div class="ascii">
+    <pre>                
+                    ..ooo.
+                 .888888888.
+                 88"P""T"T888 8o
+             o8o 8.8"8 88o."8o 8o
+            88 . o88o8 8 88."8 88P"o
+           88 o8 88 oo.8 888 8 888 88
+           88 88 88o888" 88"  o888 88
+           88."8o."T88P.88". 88888 88
+           888."888."88P".o8 8888 888
+           "888o"8888oo8888 o888 o8P"
+            "8888.""888P"P.888".88P
+             "88888ooo  888P".o888
+               ""8P"".oooooo8888P
+      .oo888ooo.    8888NICK8P8
+    o88888"888"88o.  "8888"".88   .oo888oo..
+     8888" "88 88888.       88".o88888888"888.
+     "8888o.""o 88"88o.    o8".888"888"88 "88P
+      T888C.oo. "8."8"8   o8"o888 o88" ".=888"
+       88888888o "8 8 8  .8 .8"88 8"".o888o8P
+        "8888C.o8o  8 8  8" 8 o" ...o"""8888
+          "88888888 " 8 .8  8   88888888888"
+            "8888888o  .8o=" o8o..o(8oo88"
+                "888" 88"    888888888""
+                    o8P       "888"""
+              ...oo88
+     "8oo...oo888""         [SA] 29.01.25
+    </pre>
+</div>
+
+<style>
+    .ascii {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 95vh;
+        animation: blinkFade 2s ease-in-out infinite;
+    }
+    pre {
+        color: red;
+    }
+    @keyframes blinkFade {
+        0% {
+            opacity: 1;
+        }
+        50% {
+            opacity: 0.5;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Everything Rosey</h1>
+<p>I (Je) love you. You make me smile, nicer, happy and most importantly you make me better and complete.</p>
+<style>
+    h1{
+        text-align: center;
+        font-family: monospace;
+    }
+    p {
+        margin: 0 20px;
+        font-family: monospace;
+        text-align: center;
+        font-size: 1.5em;
+    }
+</style>
